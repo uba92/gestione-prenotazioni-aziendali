c6fe54ac422cfd557f336192a1b0e1d2a624fec0
Umberto Tramontano <u.tramontano92@libero.it>
2025-02-07 10:45:33 +0100

    Initial commit

---
+++ src/main/java/it/epicode/gestione_prenotazioni_aziendali/GestionePrenotazioniAziendaliApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.gestione_prenotazioni_aziendali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionePrenotazioniAziendaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionePrenotazioniAziendaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/gestione_prenotazioni_aziendali/GestionePrenotazioniAziendaliApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.gestione_prenotazioni_aziendali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionePrenotazioniAziendaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
